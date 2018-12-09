4fbb8e22f7f920f240a61c81ad4488e90450b869
Max-Starling <17.max.starling@gmail.com>
2018-12-09 17:27:11 +0300

    Implement common button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Button.scss';
+
+const Button = ({
+  type,
+  text,
+  onClick,
+  disabled,
+}) => {
+  return (
+    <div styleName="button">
+      <button
+        type={type}
+        disabled={disabled}
+        styleName="button__inner"
+        onClick={onClick}
+      >
+        {text}
+      </button>
+    </div>
+  );
+};
+
+Button.propTypes = {
+  text: PropTypes.string,
+  type: PropTypes.string,
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
+};
+
+Button.defaultProps = {
+  text: '',
+  type: 'button',
+  disabled: false,
+};
+
+export default Button;
