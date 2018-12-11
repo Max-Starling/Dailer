8d80737dd45ea5b9ad8d0a86358f558b01a30df1
Max-Starling <17.max.starling@gmail.com>
2018-12-11 13:33:24 +0300

    Implement sidebar

---
+++ src/static/close-icon.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default ({ fill = '#000' }) => (
+  <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg">
+    <rect
+      transform="rotate(135 16.106060028076172,15.750000000000002)"
+      rx="1"
+      id="close-line-1"
+      height="1.5"
+      width="24.393997"
+      x="3.909061"
+      y="15"
+      fill={fill}
+    />
+    <rect
+      transform="rotate(45 15.954544067382816,15.749999999999998)"
+      rx="1"
+      id="close-line-2"
+      height="1.5"
+      width="24.393997"
+      x="3.757545"
+      y="15"
+      fill={fill}
+    />
+  </svg>
+);
