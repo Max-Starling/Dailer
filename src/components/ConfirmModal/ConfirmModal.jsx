e143d8ce725b65a6cc4a36713293f70bc0b00f92
Max-Starling <17.max.starling@gmail.com>
2018-12-13 23:12:25 +0300

    Implement confirm modal common component

---
+++ src/components/ConfirmModal/ConfirmModal.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+import ModalWindow from 'components/ModalWindow';
+import Button from 'components/Button';
+import './ConfirmModal.scss';
+
+const ConfirmModal = ({
+  isVisible,
+  toggleVisibility,
+  onSubmit,
+  onCancel,
+  text,
+  submitButtonText,
+}) => {
+
+  return (
+    <ModalWindow
+      toggleVisibility={toggleVisibility}
+      isVisible={isVisible}
+    >
+      <div styleName="confirm-modal">
+        <p styleName="confirm-modal__title">Confirm</p>
+        <p styleName="confirm-modal__subtitle">{text}</p>
+        <div styleName="buttons">
+          <div styleName="buttons__item">
+            <Button
+              text="Cancel"
+              onClick={onCancel ? onCancel : toggleVisibility}
+            />
+          </div>
+          <div styleName="buttons__item"> 
+            <Button
+              text={submitButtonText}
+              onClick={onSubmit}
+            />
+          </div>
+        </div>
+      </div>
+    </ModalWindow>
+  )
+};
+
+ConfirmModal.propTypes = {
+  isVisible: PropTypes.bool,
+  toggleVisibility: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  onCancel: PropTypes.func,
+  text: PropTypes.string.isRequired,
+  submitButtonText: PropTypes.string,
+};
+
+ConfirmModal.defaultProps = {
+  isVisible: false,
+  onCancel: null,
+  submitButtonText: 'OK',
+};
+
+export default ConfirmModal;
