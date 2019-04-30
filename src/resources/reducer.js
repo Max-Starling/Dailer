af4429f17df0ffaf556c33fbf032d6e5c2c9ea8c
Max-Starling <17.max.starling@gmail.com>
2019-04-30 21:39:30 +0300

    Remove all redux resources

--- src/resources/reducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import repeatableTasks from './repeatableTasks/repeatableTasks.reducer';
-
-export default combineReducers({
-  repeatableTasks,
-});
--- src/resources/repeatableTasks/repeatableTasks.selectors.js
+++
@@ -1,5 +0,0 @@
-export const getRepeatableTasks = ({ repeatableTasks }) => repeatableTasks;
-
-export const getActiveRepeatableTasks = ({ repeatableTasks }) => repeatableTasks.filter(item => item.status === 'active');
-
-export const getInactiveRepeatableTasks = ({ repeatableTasks }) => repeatableTasks.filter(item => item.status === 'inactive');
