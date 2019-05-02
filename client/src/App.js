c8bf04811d5f74cd57b3e8de1a98d66ba7a19a6f
Max-Starling <17.max.starling@gmail.com>
2019-05-02 20:47:42 +0300

    Changes: - Connect emotion-theming. - Implement fetch account settings functionality.

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,92 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { ThemeProvider } from 'emotion-theming';
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
 
 import Header from 'components/Header';
 import Content from 'components/Content';
-import RepeatableSettings from 'components/RepeatableSettings';
+import Settings from 'components/Settings';
 import RepeatableList from 'components/RepeatableList';
 import Loading from 'components/Loading';
+import themes from 'static/themes';
 import './App.css';
 
-const App = (props) => {
+const _id = '5ccaff97725968bb36279702';
+
+const query = gql`
+  query Account ($_id: ID!) {
+    account (_id: $_id) {
+      _id,
+      settings {
+        mode
+      }
+    }
+  }
+`;
+
+const App = () => {
   const [currentTab, setCurrentTab] = useState('Repeatable');
   const [isLoaded, setIsLoaded] = useState(false);
 
   const fakeLoading = () => {
-    // setTimeout(() => {
+    setTimeout(() => {
       setIsLoaded(true);
-    // }, 2000);
+    }, 1000);
   };
 
-  useEffect(fakeLoading, []);
+  // useEffect(fakeLoading, []);
 
-  const renderRoute = (Component, tab) => (props) => {
+  const renderRoute = (Component, tab, additionalProps) => (props) => {
     if (currentTab !== tab) {
       setCurrentTab(tab);
     }
-    return <Component {...props} />;
+    return <Component {...props} {...additionalProps} />;
   };
 
+  // console.log(account);
   return (
-    <Fragment>
-      <Loading isLoaded={isLoaded} />
-      <Header currentTab={currentTab} />
-      <Content>
-        <Switch>
-          <Route
-            exact
-            path="/repeatable"
-            render={renderRoute(RepeatableList, 'Repeatable')}
-          />
-          <Route
-            exact
-            path="/repeatable/settings"
-            render={renderRoute(RepeatableSettings, 'Settings')}
-          />
-          <Route
-            path="*"
-            render={() => <Redirect to={{ pathname: "/repeatable" }} />}
-          />
-          {/* <Route
-            exact
-            path="/tasks/settings"
-            component={TasksSettings}
-          /> */}
-        </Switch>
-      </Content>
-    </Fragment>
+    <Query query={query} variables={{ _id }}>
+        {({
+          loading,
+          error,
+          data: { account = { settings: {} } },
+        }) => {
+          if (loading) return null;
+          fakeLoading();
+          if (error) {
+            console.log(error);
+            return null;
+          }
+
+          const mode = account.settings.mode || 'light';
+          return (
+            <ThemeProvider theme={themes[mode]}> 
+              <Loading isLoaded={isLoaded} />
+              <Header currentTab={currentTab} />
+              <Content>
+                <Switch>
+                  <Route
+                    exact
+                    path="/repeatable"
+                    render={renderRoute(RepeatableList, 'Repeatable')}
+                  />
+                  <Route
+                    exact
+                    path="/settings"
+                    render={renderRoute(Settings, 'Settings', { account })}
+                  />
+                  <Route
+                    path="*"
+                    render={() => <Redirect to={{ pathname: "/repeatable" }} />}
+                  />
+                </Switch>
+              </Content>
+            </ThemeProvider>
+          )}
+        }
+    </Query>
   );
 }
-
 
 export default App;
