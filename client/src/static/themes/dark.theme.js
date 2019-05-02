4717e5ea6cd0339136a7c2cb6a61f8fc09b3625f
Max-Starling <17.max.starling@gmail.com>
2019-05-02 20:44:11 +0300

    Implement dark and light themes for main and menu

---
+++ client/src/static/themes/dark.theme.js
@@ -0,0 +1,17 @@
+export default ({
+  menuBackground: '#3c3c3c', // #333
+  mainBackground: '#0b6362',
+  textColor: '#f1f1f1',
+  listItem: {
+    background: '#3e3e3e',
+    boxShadow: '1px 3px 6px rgba(0,0,0,0.2), inset -3px -7px 3em 7px #2d2d2d',
+  },
+  header: {
+    background: '#6f6f6f',
+    boxShadow: '0 0 10px rgba(0,0,0,0.5), inset -20px -19px 5em 0px #3c3c3c',
+  },
+  modalWindow: {
+    background: '#353535',
+    boxShadow: '1px 3px 10px rgba(0, 0, 0, 0.4), inset -20px -19px 5em 0px #252525',
+  },
+});
