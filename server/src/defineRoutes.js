9a66b15ce391465a88ceced6a5a5447b82c61e25
Max-Starling <17.max.starling@gmail.com>
2019-05-06 17:10:42 +0300

    Implement backend login, logout routes

--- server/src/defineRoutes.js
+++ server/src/defineRoutes.js
@@ -1,7 +1,22 @@
 const Router = require('koa-router');
 const router = new Router();
 
-// router.get('/hello', () => console.log('hello'));
+router.post('/login', async (ctx) => {
+  const { email } = ctx.request.body;
+  console.log('login', ctx.session.isNew, email);
+  if (ctx.session.isNew && email) {
+    ctx.session.email = email;
+    ctx.body = `logged in with ${email}`;
+  } else {
+    ctx.body = 'already logged in';
+  }
+});
+
+router.post('/logout', async (ctx) => {
+  console.log('logout', ctx.session.email);
+  ctx.session = null;
+  ctx.body = "logged out";
+});
 
 const defineRoutes = (app) => {
   app.use(router.routes());
