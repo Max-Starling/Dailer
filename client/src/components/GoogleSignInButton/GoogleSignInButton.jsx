6282554b6d92fca45420ccd987180b667f864e03
Max-Starling <17.max.starling@gmail.com>
2019-05-06 20:37:35 +0300

    Customize Google Sign In button

--- client/src/components/GoogleSignInButton/GoogleSignInButton.jsx
+++ client/src/components/GoogleSignInButton/GoogleSignInButton.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import axios from 'axios';
+import GoogleLogin from 'react-google-login';
+import { ReactComponent as GoogleIcon } from 'static/google.svg';
+import './GoogleSignInButton.scss';
 
 const GOOGLE_BUTTON_ID = 'google-sign-in-button';
 
 class GoogleSignIn extends React.Component {
-  componentDidMount() {
-    window.gapi.signin2.render(
-      GOOGLE_BUTTON_ID,
-      {
-        width: 200,
-        height: 50,
-        onsuccess: this.onSuccess,
-        theme: 'dark',
-      },
-    );
-  }
-
   async onSuccess(googleUser) {
     const id_token = googleUser.getAuthResponse().id_token;
     const res = await axios.post(
@@ -26,9 +17,27 @@ class GoogleSignIn extends React.Component {
     console.log(res);
   }
 
+  renderButton = (renderProps) => (
+    <button
+      onClick={renderProps.onClick}
+      styleName="google-signin-button"
+    >
+      <div styleName="google-signin-button__icon"><GoogleIcon /></div>
+      <div styleName="google-signin-button__text">Sign In</div>
+    </button>
+  );
+
   render() {
     return (
-      <div id={GOOGLE_BUTTON_ID}/>
+      <>
+      <div id={GOOGLE_BUTTON_ID} />
+      <GoogleLogin
+        onSuccess={this.onSuccess}
+        render={this.renderButton}
+        buttonText='Sign In'
+        clientId='642122115167-leknpsabvq65p5vmfc9kcb72mcfg8cp5.apps.googleusercontent.com'
+      />
+      </>
     );
   }
 }
