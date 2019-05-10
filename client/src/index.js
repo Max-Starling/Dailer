3f5ee1901a104e0652a01d934f8f222933147cfa
Max-Starling <17.max.starling@gmail.com>
2019-05-10 21:14:02 +0300

    Set up websocket client

--- client/src/index.js
+++ client/src/index.js
@@ -3,20 +3,39 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from "react-apollo";
+import { split } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from 'apollo-utilities';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:5000/graphql`,
+  options: {
+    reconnect: true,
+  }
+});
 
 const httpLink = new HttpLink({
   uri: 'http://localhost:4000/graphql',
   credentials: 'include', 
 });
 
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLink,
+);
+
 const client = new ApolloClient({
-  link: httpLink,
+  link,
   cache: new InMemoryCache({
     dataIdFromObject: object => object._id || null
   }),
