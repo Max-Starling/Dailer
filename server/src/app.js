ed2b7069ac8529cd5dc4d869df165268e2218a0c
Max-Starling <17.max.starling@gmail.com>
2019-05-30 20:41:20 +0300

    Set up subscriptions server

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,18 @@
 const { ApolloServer } = require('apollo-server-koa');
+const createServer = require('http').createServer;
 const Koa = require('koa');
 const session = require('koa-session');
 const bodyParser = require('koa-bodyparser-graphql');
 const defineRoutes = require('./defineRoutes');
-require('./helpers/logger');
 const connectDB = require('./helpers/connectDB');
 const redisConfig = require('./config/redis');
 const RedisStore = require('koa-redis');
 const cors = require('@koa/cors');
+const SubscriptionServer = require('subscriptions-transport-ws').SubscriptionServer;
+const execute = require('graphql').execute;
+const makeExecutableSchema = require('graphql-tools').makeExecutableSchema;
+const subscribe = require('graphql').subscribe;
+require('./helpers/logger');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 const config = require('./config');
@@ -47,9 +52,31 @@ connectDB(() => {
     app,
     path: '/graphql',
   });
+
+  const WS_PORT = 5000;
+  const websocketServer = createServer((request, response) => {
+    response.writeHead(404);
+    response.end();
+  });
+
   app.listen({ port: config.port }, () => {
     warn(`Server listening on "${config.apiUrl}" in "${process.env.NODE_ENV}" mode`);
     warn(`Connected to GraphQL: "${config.apiUrl}${server.graphqlPath}"`);
+
+    websocketServer.listen(WS_PORT, () => {
+      warn(`Websocket Server is now running on http://localhost:${WS_PORT}`);
+      const subscriptionServer = SubscriptionServer.create(
+        {
+          schema: makeExecutableSchema({ typeDefs: GraphQLConfig.typeDefs, resolvers: GraphQLConfig.resolvers }),
+          execute,
+          subscribe,
+        },
+        {
+          server: websocketServer,
+          path: '/graphql',
+        },
+      );
+    });
   });
 });
 
--- server/src/resources/index.js
+++ server/src/resources/index.js
@@ -4,25 +4,42 @@ const mergeResolvers = require('merge-graphql-schemas').mergeResolvers;
 const AccountSchema = require('./account/account.schema');
 const RepeatableSchema = require('./repeatable/repeatable.schema');
 const TaskSchema = require('./task/task.schema');
+const MessageSchema = require('./message/message.schema');
 
-const schemaArray = [AccountSchema, TaskSchema, RepeatableSchema];
+const schemaArray = [AccountSchema, MessageSchema, TaskSchema, RepeatableSchema];
 
 const AccountResolvers = require('./account/account.resolvers');
 const RepeatableResolvers = require('./repeatable/repeatable.resolvers');
 const TaskResolvers = require('./task/task.resolvers');
+const MessageResolvers = require('./message/message.resolvers');
 
-const resolversArray = [AccountResolvers, TaskResolvers, RepeatableResolvers];
+const resolversArray = [AccountResolvers, MessageResolvers, TaskResolvers, RepeatableResolvers];
 
 module.exports = {
   typeDefs: mergeTypes(schemaArray, { all: true }),
   resolvers: mergeResolvers(resolversArray),
   context: async (props) => {
-    const ctx = props.ctx;
-    if (typeof ctx.session === 'object') {
-      return props.ctx;
+    if (props.connection) {
+      return ({
+        ...props.connection.context,
+      });
     } else {
-      warn('Session is missing');
-      return null;
+      if (typeof props.ctx.session === 'object') {
+        return props.ctx;
+      } else {
+        warn('Session is missing');
+        return null;
+      }
     }
+  },
+  uploads: false,
+  subscriptions: { // for SubscriptionClient
+    path: '/graphql',
+    onConnect: (connectionParams, webSocket, context) => {
+      console.log('onConnect', context);
+    },
+    onDisconnect: (webSocket, context) => {
+      console.log('onDisconnect', context);
+    },
   },
 };
